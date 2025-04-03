73b0ea3d8ae2e1a057498627d49da21a6ab255f2
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-02 23:36:07 -0300

    Resolucao atividade Pintando

--- ed/database/pintando/draft.go
+++ ed/database/pintando/draft.go
@@ -1,5 +1,18 @@
 package main
 import "fmt"
+import "math"
+
+func calcularArea(num1, num2, num3 float64) float64{
+    var perimetro float64 = (num1 + num2 + num3) / 2
+    var area float64 = math.Sqrt(perimetro * (perimetro - num1) * (perimetro - num2) * (perimetro - num3))
+    return area
+}
+
 func main() {
-    fmt.Println("qxcode")
+    var a, b, c float64
+
+    fmt.Scan(&a, &b, &c)
+
+    var area float64 = calcularArea(a, b, c)
+    fmt.Printf("%.2f\n", area)
 }
