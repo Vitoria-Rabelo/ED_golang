30f31334e2c9924bbe4cf38f5e2ae64a529a1854
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-03 22:58:26 -0300

    concluida atividade mumia

--- ed/database/mumia/draft.go
+++ ed/database/mumia/draft.go
@@ -1,5 +1,31 @@
 package main
+
 import "fmt"
+
+func categoria(idade int) string {
+    var x string = "mumia"
+    if idade < 12 {
+        x = "crianca"
+        return x
+    }else if idade < 18 {
+        x = "jovem"
+        return x
+    }else if idade < 65 {
+        x = "adulto"
+        return x
+    }else if idade  < 1000 {
+        x = "idoso"
+    }
+    return x
+
+}
 func main() {
-    fmt.Println("qxcode")
+    var nome string 
+    fmt.Scan(&nome)
+
+    var idade int
+    fmt.Scan(&idade)
+
+    var pessoa = categoria(idade)
+    fmt.Print(nome, " eh ", pessoa , "\n")
 }
