0f6f966d391eb4e30659362f6620985abfde5ab8
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-04 16:35:01 -0300

    Conclui atividade fuga da obi

--- ed/database/fuga/draft.go
+++ ed/database/fuga/draft.go
@@ -1,5 +1,45 @@
 package main
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+    var h, p, f, d int 
+    fmt.Scan(&h, &p, &f, &d)
+   
+    var fugiu bool
+
+    if d == -1 {
+        if h > f && h < p {
+            fugiu = true
+        }
+
+        if h < f && p > f {
+            fugiu = true
+        }
+
+        if h > p && p > f {
+            fugiu = true
+        }
+
+    }
+
+    if d == 1 {
+        if h > f && h < p {
+            fugiu = false
+        }
+
+        if h < f && p > f {
+            fugiu = false
+        }
+
+        if h > p && p > f {
+            fugiu = false
+        }
+    }
+
+    if fugiu == true {
+        fmt.Printf("S\n")
+    }else{
+        fmt.Printf("N\n")
+    }
+
 }
