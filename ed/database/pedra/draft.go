85690438c4029b4066527389cf19a55ff40058bd
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-05 20:33:41 -0300

    atividade pedra concluida

--- ed/database/pedra/draft.go
+++ ed/database/pedra/draft.go
@@ -1,5 +1,35 @@
 package main
-import "fmt"
+import (
+    "fmt"
+    "math"
+)
+
 func main() {
-    fmt.Println("qxcode")
+    var competidores int
+    fmt.Scan(&competidores)
+
+    if competidores < 1 || competidores > 1000 {
+        return
+    }
+
+    var a, b, indice int
+    menorPontuacao := 101
+    indice = -1
+
+    for i := 0; i < competidores; i++ {
+        fmt.Scan(&a, &b)
+
+        if a < 10 || b < 10 {
+            continue 
+        }
+
+        diff := int(math.Abs(float64(a - b)))
+        if diff < menorPontuacao {
+            menorPontuacao = diff
+            indice = i
+        }
+    }
+        fmt.Println(indice)
+    
 }
+
