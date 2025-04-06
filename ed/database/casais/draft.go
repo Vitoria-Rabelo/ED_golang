974703ce0c9ab5ffd7eac0f91264a8326158c2c1
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-06 09:57:32 -0300

    Conclui atividade casais

--- ed/database/casais/draft.go
+++ ed/database/casais/draft.go
@@ -1,5 +1,29 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var tam int
+    fmt.Scan(&tam)
+
+    var vector []int
+
+    for i := 0; i < tam; i++{
+        var animal int
+        fmt.Scan(&animal)
+        vector = append(vector, animal)
+    }
+
+    var qtdCasais int
+
+    for x := 0; x < tam; x++{
+        for y := 1; y < tam; y++{
+            if vector[x] != vector[y] && vector[x] == (vector[y] * -1){
+                qtdCasais++
+                vector[x] = 0
+                vector[y] = 0
+                break
+            }
+        }
+    }
+
+    fmt.Printf("%d\n", qtdCasais)
 }
