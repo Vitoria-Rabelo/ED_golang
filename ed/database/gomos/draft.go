80e42064e60bc4be9edfb762b401cd52487e8307
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-06 22:42:11 -0300

    Conclui atividade gomos

--- ed/database/gomos/draft.go
+++ ed/database/gomos/draft.go
@@ -1,5 +1,38 @@
 package main
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+    var q int
+    var d string
+    fmt.Scan(&q, &d)
+
+    var x [100]int
+    var y [100]int
+
+    for i := 0; i < q; i++ {
+        fmt.Scan(&x[i], &y[i])
+    }
+
+    oldX, oldY := x[0], y[0]
+
+    switch d {
+    case "L":
+        x[0]--
+    case "R":
+        x[0]++
+    case "U":
+        y[0]--
+    case "D":
+        y[0]++
+    }
+
+    for i := 1; i < q; i++ {
+        tempX, tempY := x[i], y[i]
+        x[i], y[i] = oldX, oldY
+        oldX, oldY = tempX, tempY
+    }
+
+    for i := 0; i < q; i++ {
+        fmt.Println(x[i], y[i])
+    }
 }
