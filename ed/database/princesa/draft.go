de217a8602755d49378977fefab889db83487c12
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-13 00:54:39 -0300

    concluido testes de atividade princesa

--- ed/database/princesa/draft.go
+++ ed/database/princesa/draft.go
@@ -1,5 +1,51 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+    var N, E int
+    fmt.Scan(&N, &E)
+
+    pessoas := make([]bool, N)
+    for i := range pessoas {
+        pessoas[i] = true
+    }
+
+    pos := E - 1
+    vivas := N
+
+    for vivas > 1 {
+        fmt.Print("[")
+        for i, viva := range pessoas {
+            if viva {
+                if i == pos {
+                    fmt.Printf(" %d>", i+1)
+                } else {
+                    fmt.Printf(" %d", i+1)
+                }
+            }
+        }
+        fmt.Println(" ]")
+
+        vitima := (pos + 1) % N
+        for !pessoas[vitima] {
+            vitima = (vitima + 1) % N
+        }
+
+        pessoas[vitima] = false
+        vivas--
+
+        pos = (vitima + 1) % N
+        for !pessoas[pos] {
+            pos = (pos + 1) % N
+        }
+    }
+
+    fmt.Print("[")
+    for i, viva := range pessoas {
+        if viva {
+            fmt.Printf(" %d>", i+1)
+        }
+    }
+    fmt.Println(" ]")
 }
