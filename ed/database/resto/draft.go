28d48c94514fb52ff0e7d3452cf9b079a9dfcf27
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-23 19:57:39 -0300

    concluida atividade resto

--- ed/database/resto/draft.go
+++ ed/database/resto/draft.go
@@ -1,5 +1,23 @@
 package main
 import "fmt"
+
+func dividir(num int){
+    if num == 0 {
+        return
+    }
+
+    resultado := num/2
+    resto := num % 2
+
+    dividir(resultado)
+
+    fmt.Println( resultado, resto)
+
+}
+
 func main() {
-    fmt.Println("qxcode")
+    var num int
+    fmt.Scanf("%v", &num)
+
+    dividir(num)
 }
