0206f2a51980e36dfcf489a71824c36f93215f65
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-23 11:37:19 -0300

    concluida atividade rotacoes

--- ed/database/rotacoes/draft.go
+++ ed/database/rotacoes/draft.go
@@ -1,5 +1,22 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var tam, rot int
+    fmt.Scan(&tam, &rot)
+
+    vector := make([]int ,tam)
+
+    for i := 0; i < tam; i++{
+        fmt.Scan(&vector[i])
+    }
+
+    newRot := rot % tam
+    fmt.Printf("[")
+    for i, _:= range vector {
+            fmt.Printf(" ")
+        indice := (i + tam - newRot) % tam
+        fmt.Printf("%v", vector[indice])
+        
+    }
+    fmt.Println(" ]")
 }
