c07054719f8a9716d31f1b18ee230b581493d17a
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-23 11:06:33 -0300

    concluida atividade buscas

--- ed/database/buscas/draft.go
+++ ed/database/buscas/draft.go
@@ -1,5 +1,37 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var tam int
+    fmt.Scan(&tam)
+
+    entrada := make([]string, tam)
+
+    for i := 0; i < tam; i++{
+        fmt.Scan(&entrada[i])
+    }
+
+    var tam2 int
+    fmt.Scan(&tam2)
+
+    consulta := make([]string, tam2)
+
+    for i := 0; i < tam2; i++{
+        fmt.Scan(&consulta[i])
+    }
+
+    for i, elem := range consulta {
+        if i != 0 {
+            fmt.Printf(" ")
+        }
+        contagem := 0
+        for _, value := range entrada {
+            if elem == value {
+                contagem++
+            }
+        }
+
+        fmt.Printf("%v", contagem)
+        
+    }
+    fmt.Println()
 }
