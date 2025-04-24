e2cf024e322ca5605c7b196130e8eedc6eeead69
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-23 22:03:58 -0300

    concluida atividade eh_primo

--- ed/database/eh_primo/main.go
+++ ed/database/eh_primo/main.go
@@ -5,13 +5,20 @@ import "fmt"
 // x: número que está sendo testado
 // div: divisor que está sendo testado
 func eh_primo(x int, div int) bool {
-	_, _ = x, div
-	return false;
+	if x == 2 || div * div > x && x != 1{
+		return true
+	}
+	if x <= 1 || x % div == 0{
+		return false
+	}
+
+	return eh_primo(x, div + 1);
 }
 
 func main() {
 	var x int
 	fmt.Scan(&x)
+
 	if eh_primo(x, 2) {
 		fmt.Println("true")
 	} else {
