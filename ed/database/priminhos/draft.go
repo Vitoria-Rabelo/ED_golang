1aa60c2339d436bf79e6a63091ff9f496c1bb160
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-04-24 23:29:50 -0300

    concluida atividade priminhos

--- ed/database/priminhos/draft.go
+++ ed/database/priminhos/draft.go
@@ -1,5 +1,42 @@
 package main
 import "fmt"
+
+func eh_primo(x int, div int) bool {
+	if x == 2 || div * div > x && x != 1{
+		return true
+	}
+	if x <= 1 || x % div == 0{
+		return false
+	}
+
+	return eh_primo(x, div + 1);
+}
+func primos(num int) []int{
+    primos := make([]int, 0, num)
+
+    num2 := 2
+
+    for len(primos) < num {
+        if eh_primo(num2, 2) {
+            primos = append(primos, num2)
+        }
+        num2++
+    }
+    return primos
+}
+
 func main() {
-    fmt.Println("qxcode")
+    var num int
+    fmt.Scan(&num)
+
+    lista := primos(num)
+    fmt.Print("[")
+    for i, elem := range lista {
+        if i > 0 {
+            fmt.Print(", ")
+        }
+        fmt.Print(elem)
+    }
+    fmt.Println("]")
+    
 }
