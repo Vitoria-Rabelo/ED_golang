2e3839eb3d0b7a728b7377fa135457679a6a1b68
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-05-03 21:57:34 -0300

    concluida atividade vetbuild

--- ed/database/vetbuild/main.go
+++ ed/database/vetbuild/main.go
@@ -15,22 +15,32 @@ type Vector struct{
 	capacity int
 }
 
-func (v *Vector) Status() string{
+func (v *Vector) Status() string {
 	return fmt.Sprintf("size:%v capacity:%v", v.size, v.capacity)
 }
 
 func (v *Vector) String() string{
-	saida := "["
+	elementos := "["
+
 	for i := range v.size{
-		if i != 0{
-			saida += ", "
+		if i != 0 {
+			elementos += ", "
 		}
-		saida += fmt.Sprint(v.data[i])
+		elementos += fmt.Sprint(v.data[i])
 	}
-	return saida + "]"
+	elementos += "]"
+	return elementos
 }
 
-func NewVector(capacity int) *Vector{
+func (v *Vector) GetSize() int{
+	return v.size
+}
+
+func (v *Vector) Capacity() int{
+	return v.capacity
+}
+
+func NewVector(capacity int) * Vector {
 	return &Vector{
 		size: 0,
 		data: make([]int, capacity),
@@ -38,28 +48,30 @@ func NewVector(capacity int) *Vector{
 	}
 }
 
-func (v *Vector) GetSize() int{
-	return v.size
-}
-
-func (v *Vector) PushBack(value int){
-	if v.size == v.capacity {
-		v.Reserve(max(1, v.capacity*2))
-	}
-	v.data[v.size] = value
-	v.size++
-}
-
-func (v *Vector) Reserve(newCapacity int){
+func (v *Vector) Reserve(newCapacity int) {
 	if newCapacity > v.capacity{
 		newData := make([]int, newCapacity)
+
 		copy(newData, v.data)
 		v.data = newData
 		v.capacity = newCapacity
 	}
+
+}
+func (v *Vector) PushBack(value int){
+	if v.size == v.capacity {
+		v.Reserve(max(1, v.capacity*2))
+	}
+
+	v.data[v.size] = value
+	v.size++
 }
 
-func (v *Vector) PopBack() error{
+func (v *Vector) Clear() {
+	 v.size = 0
+}
+
+func (v * Vector) PopBack()  error{
 	if v.size == 0{
 		return errors.New("vector is empty")
 	}
@@ -67,24 +79,104 @@ func (v *Vector) PopBack() error{
 	return nil
 }
 
-func (v * Vector) Insert(value int, index int) error {
-	if index < 0 || index > v.size {
+func (v *Vector) Insert(value int, index int) error{
+	if index < 0 || index > v.size{
 		return fmt.Errorf("index out of size")
 	}
 
-	if v.size == v.capacity {
-		v.Reserve(max(1, v,capacity*2))
+	if v.size == v.capacity{
+		v.Reserve(max(1, v.capacity * 2))
 	}
 
-	for i := v.size -1; v 
+	for i := v.size; i > index; i--{
+		v.data[i] = v.data[i - 1]
+	}
 
-
+	v.data[index] = value
+	v.size++
+	return nil
 }
 
-func (v * Vector) Clear() {
-	v.size = 0
+func (v *Vector) Get(index int) (int, error) {
+	if index < 0 || index >= v.size {
+		return 0, errors.New("index out of range")
+	}
+	return v.data[index], nil
 }
 
+func (v * Vector) Set(index int, value int) error{
+	if index < 0 || index >= v.size {
+        return errors.New("index out of range")
+    }
+	v.data[index] = value
+	return nil
+}
+
+func (v * Vector) Erase(index int) error{
+	if index < 0 || index >= v.size {
+        return errors.New("index out of range")
+    }
+
+	for i := index; i < v.size-1; i++ {
+        v.data[i] = v.data[i+1]
+    }
+	v.size--
+
+	return nil
+}
+
+func (v * Vector) IndexOf(value int) int{
+	for i := 0; i < v.size; i++{
+		if value == v.data[i]{
+			return i
+		}
+	}
+	return -1
+}
+
+func (v * Vector) Contains(value int) bool{
+	for i := 0; i < v.size; i++{
+		if value == v.data[i]{
+			return true
+		}
+	}
+	return false
+}
+
+func (v *Vector) Reverse() {
+	for i, j := 0, v.size-1; i < j; i, j = i+1, j-1 {
+        v.data[i], v.data[j] = v.data[j], v.data[i]
+    }
+}
+
+func (v * Vector) Sort(){
+    for i := 0; i < v.size -1; i++ {
+        for j := 0; j < v.size - i - 1; j++ {
+            if v.data[j] > v.data[j+1] {
+                v.data[j], v.data[j+1] = v.data[j+1], v.data[j]
+            }
+        }
+    }
+}
+
+func (v *Vector) Sorted() *Vector {
+
+    new := NewVector(v.capacity)
+    new.size = v.size
+
+    for i := 0; i < v.size; i++ {
+        new.data[i] = v.data[i]
+    }
+    for i := 0; i < new.size-1; i++ {
+        for j := 0; j < new.size-i-1; j++ {
+            if new.data[j] > new.data[j+1] {
+                new.data[j], new.data[j+1] = new.data[j+1], new.data[j]
+            }
+        }
+    }
+    
+    return new
+}
 func main() {
 	var line, cmd string
 	scanner := bufio.NewScanner(os.Stdin)
@@ -124,57 +216,58 @@ func main() {
 				fmt.Println(err)
 			}
 		case "insert":
-			// index, _ := strconv.Atoi(parts[1])
-			// value, _ := strconv.Atoi(parts[2])
-			// if err != nil {
-			// 	fmt.Println(err)
-			// }
+			index, _ := strconv.Atoi(parts[1])
+			value, _ := strconv.Atoi(parts[2])
+			err := v.Insert(value, index)
+			if err != nil {
+				fmt.Println(err)
+			}
 		case "erase":
-			// index, _ := strconv.Atoi(parts[1])
-			// err := v.Erase(index)
-			// if err != nil {
-			// 	fmt.Println(err)
-			// }
+			index, _ := strconv.Atoi(parts[1])
+			err := v.Erase(index)
+			if err != nil {
+				fmt.Println(err)
+			}
 		case "indexOf":
-			// value, _ := strconv.Atoi(parts[1])
-			// index := v.IndexOf(value)
-			// fmt.Println(index)
+			value, _ := strconv.Atoi(parts[1])
+			index := v.IndexOf(value)
+			fmt.Println(index)
 		case "contains":
-			// value, _ := strconv.Atoi(parts[1])
-			// if v.Contains(value) {
-			// 	fmt.Println("true")
-			// } else {
-			// 	fmt.Println("false")
-			// }
+			value, _ := strconv.Atoi(parts[1])
+			if v.Contains(value) {
+			 	fmt.Println("true")
+			} else {
+			 	fmt.Println("false")
+			}
 		case "clear":
 			v.Clear()
 		case "capacity":
-			// fmt.Println(v.Capacity())
+		 fmt.Println(v.Capacity())
 		case "get":
-			// index, _ := strconv.Atoi(parts[1])
-			// value, err := v.At(index)
-			// if err != nil {
-			// 	fmt.Println(err)
-			// } else {
-			// 	fmt.Println(value)
-			// }
+			index, _ := strconv.Atoi(parts[1])
+			value, err := v.Get(index)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(value)
+			}
 		case "set":
-			// index, _ := strconv.Atoi(parts[1])
-			// value, _ := strconv.Atoi(parts[2])
-			// err := v.Set(index, value)
-			// if err != nil {
-			// 	fmt.Println(err)
-			// }
-			// 
+			index, _ := strconv.Atoi(parts[1])
+			value, _ := strconv.Atoi(parts[2])
+			err := v.Set(index, value)
+			 if err != nil {
+				fmt.Println(err)
+			 }
 		case "reserve":
 			newCapacity, _ := strconv.Atoi(parts[1])
 			v.Reserve(newCapacity)
 		case "sort":
-			// v.Sort()
+			v.Sort()
 		case "sorted":
-			// fmt.Println("[" + Join(v.Sorted(), ", ") + "]")
+			sorted := v.Sorted()
+    		fmt.Println(sorted)
 		case "reverse":
-			// v.Reverse()
+			v.Reverse()
 		default:
 			fmt.Println("fail: comando invalido")
 		}
