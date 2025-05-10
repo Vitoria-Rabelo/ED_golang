4661e0dc68dbc73bbc9e556823f2c1bb00e33f06
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-05-10 00:29:44 -0300

    atividde iterador concluida

--- ed/database/iterador/main.go
+++ ed/database/iterador/main.go
@@ -17,66 +17,135 @@ type Iterator struct {
 	index int
 }
 
+type ReverseIterator struct {
+	Iterator
+}
+
+type CyclicIterator struct {
+	Iterator
+	max     int
+	counter int
+}
+
 func NewMyList(values []int) *MyList {
 	return &MyList{data: values}
 }
 
 func (l *MyList) Iterator() *Iterator {
-	return &Iterator{data: l.data, index: -1}
+	return &Iterator{data: l.data, index: 0}
 }
 
 func (i *Iterator) HasNext() bool {
-	return i.index < len(i.data)-1
+	return i.index < len(i.data)
 }
 
 func (i *Iterator) Next() int {
-	if i.index == len(i.data) {
-		panic(fmt.Errorf("No more elements"))
+	if !i.HasNext() {
+		panic("No more elements")
 	}
-	i.index += 1
-	return i.data[i.index]
+	value := i.data[i.index]
+	i.index++
+	return value
+}
+
+func (l *MyList) ReverseIterator() *ReverseIterator {
+	return &ReverseIterator{
+		Iterator: Iterator{
+			data:  l.data,
+			index: len(l.data) - 1,
+		},
+	}
+}
+
+func (ri *ReverseIterator) HasNext() bool {
+	return ri.index >= 0
+}
+
+func (ri *ReverseIterator) Next() int {
+	if !ri.HasNext() {
+		panic("No more elements")
+	}
+	value := ri.data[ri.index]
+	ri.index--
+	return value
+}
+
+func (l *MyList) CyclicIterator(max int) *CyclicIterator {
+	return &CyclicIterator{
+		Iterator: Iterator{
+			data:  l.data,
+			index: 0,
+		},
+		max: max,
+	}
+}
+
+func (ci *CyclicIterator) HasNext() bool {
+	return ci.counter < ci.max
+}
+
+func (ci *CyclicIterator) Next() int {
+	if len(ci.data) == 0 {
+		panic("Empty list")
+	}
+	if !ci.HasNext() {
+		panic("No more elements")
+	}
+	value := ci.data[ci.index]
+	ci.index = (ci.index + 1) % len(ci.data)
+	ci.counter++
+	return value
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	mylist := NewMyList([]int{})
+
 	for scanner.Scan() {
 		line := scanner.Text()
 		args := strings.Fields(line)
+		if len(args) == 0 {
+			continue
+		}
 		fmt.Println("$" + line)
 
 		switch args[0] {
 		case "end":
-			break
+			return
 		case "read":
-			for i := 1; i < len(args); i++ {
-				slice := make([]int, len(args)-1)
-				for i, value := range args[1:] {
-					slice[i], _ = strconv.Atoi(value)
-				}
-				mylist = NewMyList(slice)
+			slice := make([]int, len(args)-1)
+			for i, value := range args[1:] {
+				num, _ := strconv.Atoi(value)
+				slice[i] = num
 			}
+			mylist = NewMyList(slice)
 		case "show":
 			fmt.Print("[ ")
 			for it := mylist.Iterator(); it.HasNext(); {
-				fmt.Printf("%v ", it.Next())
+				fmt.Print(it.Next(), " ")
 			}
 			fmt.Println("]")
 		case "reverse":
-			// fmt.Print("[ ")
-			// for it := mylist.ReverseIterator(); it.HasNext(); {
-			// 	fmt.Printf("%v ", it.Next())
-			// }
-			// fmt.Println("]")
+			fmt.Print("[ ")
+			for it := mylist.ReverseIterator(); it.HasNext(); {
+				fmt.Print(it.Next(), " ")
+			}
+			fmt.Println("]")
 		case "cyclic":
-			// qtd, _ := strconv.Atoi(args[1])
-			// fmt.Print("[ ")
-			// it := mylist.CyclicIterator()
-			// for range qtd {
-			// 	fmt.Printf("%v ", it.Next())
-			// }
-			// fmt.Println("]")
+			if len(args) < 2 {
+				fmt.Println("Usage: cyclic <max_iterations>")
+				continue
+			}
+			max, err := strconv.Atoi(args[1])
+			if err != nil {
+				fmt.Println("Invalid number of iterations")
+				continue
+			}
+			fmt.Print("[ ")
+			for it := mylist.CyclicIterator(max); it.HasNext(); {
+				fmt.Print(it.Next(), " ")
+			}
+			fmt.Println("]")
 		}
 	}
-
 }
