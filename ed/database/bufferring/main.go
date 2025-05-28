f17e0e650ce8f6386d764fa082a4ed6f94eaf93f
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-05-28 11:03:09 -0300

    concluida atividade buffer

--- ed/database/bufferring/main.go
+++ ed/database/bufferring/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors" // Adicionei esta importação
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,7 +18,7 @@ type Deque struct {
 
 func (b *Deque) String() string {
 	result := []string{}
-	for i := range b.size {
+	for i := 0; i < b.size; i++ { 
 		val := b.data[(b.front+i)%b.capacity]
 		result = append(result, fmt.Sprint(val))
 	}
@@ -25,13 +27,13 @@ func (b *Deque) String() string {
 
 func (b *Deque) Debug() string {
 	result := make([]string, b.capacity)
-	for i, _ := range result {
+	for i := range result {
 		result[i] = " _"
 		if i == b.front {
 			result[i] = ">_"
 		}
 	}
-	for i := range b.size {
+	for i := 0; i < b.size; i++ { 
 		index := (b.front + i) % b.capacity
 		val := b.data[index]
 		prefix := " "
@@ -41,6 +43,77 @@ func (b *Deque) Debug() string {
 		result[index] = fmt.Sprintf("%s%d", prefix, val)
 	}
 	return strings.Join(result, " |")
+}
+
+func (b *Deque) Len() int {
+	if b == nil {
+		return 0
+	}
+	return b.size
+}
+
+func (b *Deque) Front() (int, error) {
+	if b.size == 0 {
+		return 0, errors.New("empty deque")
+	}
+	return b.data[b.front], nil
+}
+
+func (b *Deque) Back() (int, error) {
+	if b.size == 0 {
+		return 0, errors.New("empty deque")
+	}
+	return b.data[(b.front+b.size-1)%b.capacity], nil
+}
+
+func (b *Deque) PushBack(value int) {
+	if b.size == b.capacity {
+		b.resize(2 * b.capacity)
+	}
+	b.data[(b.front+b.size)%b.capacity] = value
+	b.size++
+}
+
+func (b *Deque) PushFront(value int) {
+	if b.size == b.capacity {
+		b.resize(2 * b.capacity)
+	}
+	b.front = (b.front - 1 + b.capacity) % b.capacity
+	b.data[b.front] = value
+	b.size++
+}
+
+	if b.size == 0 {
+		return errors.New("fail: buffer vazio")
+	}
+	b.data[b.front] = 0
+	b.front = (b.front + 1) % b.capacity
+	b.size--
+	return nil
+}
+
+func (b *Deque) PopBack() error { 
+	if b.size == 0 {
+		return errors.New("fail: buffer vazio")
+	}
+	b.data[(b.front+b.size-1)%b.capacity] = 0
+	b.size--
+	return nil
+}
+
+func (b *Deque) Clear() { 
+	b.front = 0
+	b.size = 0
+}
+
+func (b *Deque) resize(newCapacity int) {
+	newData := make([]int, newCapacity)
+	for i := 0; i < b.size; i++ {
+		newData[i] = b.data[(b.front+i)%b.capacity]
+	}
+	b.data = newData
+	b.front = 0
+	b.capacity = newCapacity
 }
 
 func main() {
@@ -68,39 +141,39 @@ func main() {
 		case "debug":
 			fmt.Println(buf.Debug())
 		case "size":
-			// fmt.Println(buf.Len())
+			fmt.Println(buf.Len())
 		case "push_back":
-			// for _, v := range args[1:] {
-			// 	num, _ := strconv.Atoi(v)
-			// 	buf.PushBack(num)
-			// }
+			for _, v := range args[1:] {
+				num, _ := strconv.Atoi(v)
+				buf.PushBack(num)
+			}
 		case "push_front":
-			// for _, v := range args[1:] {
-			// 	num, _ := strconv.Atoi(v)
-			// 	buf.PushFront(num)
-			// }
+			for _, v := range args[1:] {
+				num, _ := strconv.Atoi(v)
+				buf.PushFront(num)
+			}
 		case "pop_back":
-			// if err := buf.PopBack(); err != nil {
-			// 	fmt.Println(err)
-			// }
+			if err := buf.PopBack(); err != nil {
+				fmt.Println(err)
+			}
 		case "pop_front":
-			// if err := buf.PopFront(); err != nil {
-			// 	fmt.Println(err)
-			// }
+			if err := buf.PopFront(); err != nil {
+				fmt.Println(err)
+			}
 		case "front":
-			// if val, err := buf.Front(); err != nil {
-			// 	fmt.Println(err)
-			// } else {
-			// 	fmt.Println(val)
-			// }
+			if val, err := buf.Front(); err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(val)
+			}
 		case "back":
-			// if val, err := buf.Back(); err != nil {
-			// 	fmt.Println(err)
-			// } else {
-			// 	fmt.Println(val)
-			// }
+			if val, err := buf.Back(); err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(val)
+			}
 		case "clear":
-			// buf.Clear()
+			buf.Clear()
 		case "end":
 			return
 		default:
