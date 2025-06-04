5cdac8239541eb94135e57d416d3049a4525d69e
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-06-03 21:08:15 -0300

    concluida atividade bettersearcj

---
+++ ed/database/bettersearch/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func BetterSearch(slice []int, value int) (bool, int) {
+	for i, v := range slice {
+        if v == value {
+            return true, i
+
+        }
+	}
+
+	for i, v := range slice {
+        if v > value {
+            return false, i  
+        }
+		if i == len(slice) - 1 && v < value{
+			return false, i + 1
+		}
+	}
+
+	return false, 0
+}
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	parts := strings.Split(scanner.Text(), " ")
+	slice := []int{}
+	for _, elem := range parts[1 : len(parts)-1] {
+		value, _ := strconv.Atoi(elem)
+		slice = append(slice, value)
+	}
+	scanner.Scan()
+	value, _ := strconv.Atoi(scanner.Text())
+	found, result := BetterSearch(slice, value)
+	if found {
+		fmt.Println("V", result)
+	} else {
+		fmt.Println("F", result)
+	}
+}
