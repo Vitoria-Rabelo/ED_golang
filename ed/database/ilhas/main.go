e193883a2f39408efeb6fcbd7b9dccb69520892c
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-06-19 22:10:55 -0300

    concluida atividade ilha

--- ed/database/ilhas/main.go
+++ ed/database/ilhas/main.go
@@ -9,9 +9,36 @@ import (
 // Não modifique a assinatura da função numIslands
 // Ela é a função que será chamada no LeetCode para resolver o problema
 func numIslands(grid [][]byte) int {
-	//
-	_ := grid
-	return 0
+	nl := len(grid)
+	nc := len(grid[0])
+	
+	if len(grid) == 0{
+		return 0
+	}
+	
+	cont := 0
+	for i := 0; i < nl; i++{
+		for j := 0; j < nc; j++{
+			if grid[i][j] == '1'{
+				cont++
+				dfs(grid, i, j, nl, nc)
+			}
+		}
+	}
+	return cont
+	
+}
+
+func dfs(grid[][] byte, i , j , nl, nc int) {
+	if i >= nl || j >= nc || i < 0 || j < 0 || grid[i][j] == '0'{
+		return 
+	}
+	grid[i][j] = '0'
+	dfs(grid, i + 1,j,  nl, nc)
+	dfs(grid, i, j + 1, nl, nc )
+	dfs(grid, i - 1, j, nl, nc)
+	dfs(grid, i, j- 1, nl, nc)
+
 }
 
 // Não modifique a função main
