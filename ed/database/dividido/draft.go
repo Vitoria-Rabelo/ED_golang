46adb8c5df9d70fdc76f2cec1d1c4fac5c0198d8
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-07-02 23:08:15 -0300

    concluida atividade dividido

--- ed/database/dividido/draft.go
+++ ed/database/dividido/draft.go
@@ -1,5 +1,77 @@
+
 package main
-import "fmt"
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Node struct {
+	value int
+	left  *Node
+	right *Node
+}
+
+type Tree struct {
+	root *Node
+}
+
+func NewNode(value int) *Node {
+	return &Node{
+		value: value,
+		left:  nil,
+		right: nil,
+	}
+}
+
+func NewTree() *Tree {
+	return &Tree{
+		root: nil,
+	}
+}
+
+func deserialize(scanner *bufio.Scanner) *Node {
+	if !scanner.Scan() {
+		return nil
+	}
+	token := scanner.Text()
+	if token == "#" {
+		return nil
+	}
+	value, err := strconv.Atoi(token)
+	if err != nil {
+		return nil
+	}
+	n := NewNode(value)
+	n.left = deserialize(scanner)
+	n.right = deserialize(scanner)
+	return n
+}
+
+func show(n *Node) {
+	if n == nil {
+		return
+	}
+
+	show(n.left)
+	fmt.Printf("%d ", n.value)
+	show(n.right)
+}
+
 func main() {
-    fmt.Println("qxcode")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := strings.TrimSpace(scanner.Text())
+
+	bt := NewTree()
+	scanner = bufio.NewScanner(strings.NewReader(input))
+	scanner.Split(bufio.ScanWords)
+	bt.root = deserialize(scanner)
+
+	fmt.Print("[ ")
+	show(bt.root)
+	fmt.Println("]")
 }
