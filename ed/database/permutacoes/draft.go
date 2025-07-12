24d0d78eff71136700f375633f7448d78d4f565b
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-07-12 20:53:05 -0300

    concluida atividade permutacoes

--- ed/database/permutacoes/draft.go
+++ ed/database/permutacoes/draft.go
@@ -1,5 +1,31 @@
 package main
 import "fmt"
+import "sort"
+
+func permutar(s []rune, l, r int, resultados*[]string){
+    if l == r{
+        *resultados = append(*resultados,string(s))
+    }else{
+        for i := l; i <= r; i++{
+            s[l], s[i] = s[i], s[l]
+            permutar(s, l+1, r, resultados)
+            s[l], s[i] = s[i], s[l]
+        }
+    }
+}
 func main() {
-    fmt.Println("qxcode")
+    var entrada string
+    fmt.Scan(&entrada)
+
+    caracteres := []rune(entrada)
+    var resultados []string
+
+    permutar(caracteres, 0, len(caracteres)-1, &resultados)
+
+   sort.Strings(resultados)
+
+    for _, perm := range resultados {
+        fmt.Println(perm)
+    }
+
 }
